d3f3a7cda8e7414a3022ec9d1dabc4f5ab1515c0
alexisholyoak <peraltaholyoak.aj@gmail.com>
2017-08-14 22:35:48 -0500

    crud login

--- main.py
+++ main.py
@@ -17,7 +17,11 @@ class Login(QtGui.QWidget, login):
 		self.BTN_INGRESAR.clicked.connect(self.ValidarUsuario)
 
 	def ValidarUsuario(self):
-		pass
+		 import hashlib
+		 tmp = hashlib.sha512("alex").hexdigest()
+#35f319ca1dfc9689f5a33631c8f93ed7c3120ee7afa05b1672c7df7b71f63a6753def5fd3ac9db2eaf90ccab6bff31a486b51c7095ff958d228102b84efd7736
+
+		 self.TXTUSUARIO.setText(tmp)
 if __name__=="__main__":
 	 app=QtGui.QApplication(sys.argv)
 	 FWindows = Login(None)
